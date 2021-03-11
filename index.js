50fa98b6e5bbff45a0e269f1bab509c9db0e25dc
Enes <enesyunlu@gmail.com>
2021-03-11 17:13:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { RNPinningSsl } = NativeModules;
 
 export async function isSSLValid({url, hashes, domainNames}) {
   try {
-    const isValid = await RNPinningSsl.getStatus(url, hashes, domainNames);
+    const isValid = await RNPinningSsl?.getStatus(url, hashes, domainNames);
     return isValid;
   } catch (e) {
     throw new Error(e);
